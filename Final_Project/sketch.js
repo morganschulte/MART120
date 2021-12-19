467094bb84eb653f17e389ec960dd10d7a35f2e6
Morgan Schulte <60452807+morganschulte@users.noreply.github.com>
2021-12-18 17:23:40 -0700

    Add files via upload

---
+++ Final_Project/sketch.js
@@ -0,0 +1,680 @@
+function setup()
+{
+  createCanvas(600,800);
+}
+function draw()
+{
+  background(54,73,88);
+  shape01();
+  shape02();
+  shape03();
+  shape04();
+  shape05();
+  shape06();
+  shape07();
+  shape08();
+  shape09();
+  shape10();
+  shape11();
+  shape12();
+  shape13();
+  shape14();
+  shape15();
+  shape16();
+  shape17();
+  shape18();
+  shape19();
+  shape20();
+  shape21();
+  shape22();
+  shape23();
+  shape24();
+  shape25();
+  shape26();
+  shape27();
+  shape28();
+  shape29();
+  shape30();
+  shape31();
+  shape32();
+  shape33();
+  shape34();
+  shape35();
+  shape36();
+  shape37();
+  shape38();
+  shape39();
+  shape40();
+  shape41();
+  shape42();
+  shape43();
+  shape44();
+  shape45();
+  shape46();
+  shape47();
+  shape48();
+  shape49();
+  shape50();
+  shape51();
+  shape52();
+  shape53();
+  shape54();
+  shape55();
+  shape56();
+  shape57();
+  shape58();
+  shape59();
+  shape60();
+  shape61();
+  shape62();
+  shape63();
+  shape64();
+  shape65();
+  shape66();
+  shape67();
+  shape68();
+  shape69();
+  shape70();
+  shape71();
+  shape72();
+  shape73();
+  shape74();
+  shape75();
+  shape76();
+  shape77();
+  shape78();
+  shape79();
+  shape80();
+  shape81();
+  shape82();
+  shape83();
+  shape84();
+  shape85();
+  shape86();
+  shape87();
+  shape88();
+  shape89();
+  shape90();
+  shape91();
+  shape92();
+  shape93();
+  shape94();
+  shape95();
+  shape96();
+}
+function shape01()
+{
+noStroke();
+fill(80,62,38);
+quad(241,121,214,166,140,192,171,155);
+}
+function shape02()
+{
+noStroke();
+fill(101,82,59);
+quad(179,178,139,294,120,280,136,194);
+}
+function shape03()
+{
+noStroke();
+fill(80,52,26);
+quad(180,177,213,166,181,226,153,253);
+}
+function shape04()
+{
+  noStroke();
+  fill(133,109,83);
+  quad(136,196,82,428,74,321,116,218);
+}
+function shape05()
+{
+  noStroke();
+  fill(119,92,64);
+  quad(118,282,138,294,140,394,82,427);
+}
+function shape06()
+{
+  noStroke();
+  fill(126,112,93);
+  quad(140,294,182,345,189,453,141,392);
+}
+function shape07()
+{
+  noStroke();
+  fill(93,67,41);
+  quad(151,255,201,272,182,344,138,294);
+}
+function shape08()
+{
+  noStroke();
+  fill(15,12,18);
+  quad(182,227,226,243,201,273,151,256);
+}
+function shape09()
+{
+  noStroke();
+  fill(182,176,176);
+  quad(201,244,199,250,187,251,187,245);
+}
+function shape10()
+{
+  noStroke();
+  fill(180,150,123);
+  quad(180,226,214,166,243,175,252,250);
+}
+function shape11()
+{
+  noStroke();
+  fill(155,132,108);
+  quad(215,166,241,122,263,123,243,173);
+}
+function shape12()
+{
+  noStroke();
+  fill(191,159,129);
+  quad(263,123,243,173,253,250,286,200);
+}
+function shape13()
+{
+  noStroke();
+  fill(122,95,66);
+  quad(264,123,324,122,321,151,286,199);
+}
+function shape14()
+{
+  noStroke();
+  fill(172,157,137);
+  quad(324,122,322,151,345,171,353,123);
+}
+function shape15()
+{
+  noStroke();
+  fill(166,128,86);
+  quad(264,234,321,152,345,171,299,236);
+}
+function shape16()
+{
+  noStroke();
+  fill(226,206,188);
+  triangle(412,207,454,218,470,341);
+}
+function shape17()
+{
+  noStroke();
+  fill(220,192,170);
+  quad(369,258,431,250,402,339,375,313);
+}
+function shape18()
+{
+  noStroke();
+  fill(133,116,95);
+  triangle(206,563,144,395,189,454);
+}
+function shape19()
+{
+  noStroke();
+  fill(75,49,24);
+  quad(182,346,201,272,228,240,253,250);
+}
+function shape20()
+{
+  noStroke();
+  fill(149,109,64);
+  quad(233,280,264,232,299,235,300,255);
+}
+function shape21()
+{
+  noStroke();
+  fill(211,203,194);
+  triangle(374,157,406,141,445,173);
+}
+function shape22()
+{
+  noStroke();
+  fill(217,179,140);
+  triangle(341,281,401,339,379,407);
+}
+function shape23()
+{
+  noStroke();
+  fill(60,38,17);
+  quad(189,451,182,345,240,277,243,334);
+}
+function shape24()
+{
+  noStroke();
+  fill(187,153,121);
+  triangle(299,237,344,170,341,236);
+}
+function shape25()
+{
+  noStroke();
+  fill(197,160,130);
+  triangle(385,209,412,206,430,248);
+}
+function shape26()
+{
+  noStroke();
+  fill(213,170,126);
+  quad(469,337,460,259,482,266,497,323);
+}
+function shape27()
+{
+  noStroke();
+  fill(205,166,133);
+  quad(492,371,523,341,547,378,532,415);
+}
+function shape28()
+{
+  noStroke();
+  fill(187,174,161);
+  quad(102,587,103,447,160,515,263,712);
+}
+function shape29()
+{
+  noStroke();
+  fill(227,217,207);
+  quad(427,797,360,622,400,586,433,625);
+}
+function shape30()
+{
+  noStroke();
+  fill(56,32,11);
+  quad(244,336,242,280,299,258,301,332);
+}
+function shape31()
+{
+  noStroke();
+  fill(209,176,147);
+  triangle(301,239,345,259,375,314);
+}
+function shape32()
+{
+  noStroke();
+  fill(229,176,147);
+  quad(343,220,345,173,376,157,410,205);
+}
+function shape33()
+{
+  noStroke();
+  fill(229,207,189);
+  quad(460,261,476,193,502,234,482,266);
+}
+function shape34()
+{
+  noStroke();
+  fill(230,209,192);
+  quad(491,369,495,324,510,319,522,341);
+}
+function shape35()
+{
+  noStroke();
+  fill(11,10,9);
+  quad(230,414,254,374,272,383,248,423);
+}
+function shape36()
+{
+  noStroke();
+  fill(20,19,15);
+  quad(293,398,304,368,338,391,316,409);
+}
+function shape37()
+{
+  noStroke();
+  fill(107,69,70);
+  quad(228,490,285,457,299,544,269,535);
+}
+function shape38()
+{
+  noStroke();
+  fill(128,102,104);
+  quad(289,561,338,546,352,560,300,581);
+}
+function shape39()
+{
+  noStroke();
+  fill(126,113,104);
+  quad(261,613,261,586,332,589,350,613);
+}
+function shape40()
+{
+  noStroke();
+  fill(187,176,166);
+  quad(148,798,134,684,260,710,310,798);
+}
+function shape41()
+{
+  noStroke();
+  fill(228,224,220);
+  quad(426,799,433,625,527,754,517,798);
+}
+function shape42()
+{
+  noStroke();
+  fill(180,143,103);
+  quad(300,331,300,240,340,280,357,336);
+}
+function shape43()
+{
+  noStroke();
+  fill(87,64,44);
+  quad(224,492,189,454,217,389,250,477);
+}
+function shape44()
+{
+  noStroke();
+  fill(182,161,142);
+  quad(319,474,344,373,391,453,361,492);
+}
+function shape45()
+{
+  noStroke();
+  fill(51,39,37);
+  quad(260,586,255,548,289,561,303,588);
+}
+function shape46()
+{
+  noStroke();
+  fill(206,181,161);
+  quad(351,615,331,589,360,569,369,605);
+}
+function shape47()
+{
+  noStroke();
+  fill(205,197,189);
+  triangle(133,685,103,586,264,713);
+}
+function shape48()
+{
+  noStroke();
+  fill(228,224,220);
+  quad(424,799,432,625,526,755,517,799);
+}
+function shape49()
+{
+  noStroke();
+  fill(210,189,170);
+  triangle(506,445,469,351,531,414);
+}
+function shape50()
+{
+  noStroke();
+  fill(228,202,180);
+  quad(545,307,494,321,480,265,498,229);
+}
+function shape51()
+{
+  noStroke();
+  fill(158,141,122);
+  triangle(104,447,143,395,203,560);
+}
+function shape52()
+{
+  noStroke();
+  fill(25,21,14);
+  quad(200,521,191,456,224,486,233,554);
+}
+function shape53()
+{
+  noStroke();
+  fill(86,69,66);
+  triangle(300,583,353,569,329,591);
+}
+function shape54()
+{
+  noStroke();
+  fill(233,219,206);
+  quad(401,584,379,408,432,255,508,444);
+}
+function shape55()
+{
+  noStroke();
+  fill(226,193,164);
+  quad(469,351,468,335,493,324,491,369);
+}
+function shape56()
+{
+  noStroke();
+  fill(202,188,176);
+  triangle(213,620,158,510,261,619);
+}
+function shape57()
+{
+  noStroke();
+  fill(225,217,208);
+  quad(309,797,263,715,378,688,427,797);
+}
+function shape58()
+{
+  noStroke();
+  fill(234,231,227);
+  quad(525,757,398,588,565,587,560,706);
+}
+function shape59()
+{
+  noStroke();
+  fill(16,13,9);
+  quad(218,390,244,335,270,354,229,417);
+}
+function shape60()
+{
+  noStroke();
+  fill(59,48,42);
+  triangle(279,387,306,360,294,398);
+}
+function shape61()
+{
+  noStroke();
+  fill(36,35,31);
+  quad(284,441,333,414,320,472,286,456);
+}
+function shape62()
+{
+  noStroke();
+  fill(58,33,24);
+  quad(242,524,225,523,222,483,242,506);
+}
+function shape63()
+{
+  noStroke();
+  fill(173,144,141);
+  quad(256,548,268,536,293,545,294,560);
+}
+function shape64()
+{
+  noStroke();
+  fill(228,215,208);
+  triangle(326,547,347,504,341,545);
+}
+function shape65()
+{
+  noStroke();
+  fill(73,60,53);
+  quad(356,516,360,491,391,452,385,518);
+}
+function shape66()
+{
+  noStroke();
+  fill(219,205,193);
+  triangle(546,375,507,312,547,307);
+}
+function shape67()
+{
+  noStroke();
+  fill(228,221,214);
+  quad(211,615,361,610,378,689,263,713);
+}
+function shape68()
+{
+  noStroke();
+  fill(228,202,183);
+  triangle(398,585,531,415,564,585);
+}
+function shape69()
+{
+  noStroke();
+  fill(122,95,66);
+  triangle(216,799,223,767,228,799);
+}
+function shape70()
+{
+  noStroke();
+  fill(178,133,88);
+  quad(299,240,343,237,370,260,376,310);
+}
+function shape71()
+{
+  noStroke();
+  fill(23,17,12);
+  quad(351,246,383,209,419,238,367,261);
+}
+function shape72()
+{
+  noStroke();
+  fill(218,195,175);
+  triangle(446,174,459,260,475,191);
+}
+function shape73()
+{
+  noStroke();
+  fill(27,20,13);
+  quad(242,334,319,331,337,362,267,352);
+}
+function shape74()
+{
+  noStroke();
+  fill(47,42,36);
+  quad(303,370,336,361,343,372,339,391);
+}
+function shape75()
+{
+  noStroke();
+  fill(25,25,22);
+  quad(247,423,270,380,280,386,283,441);
+}
+function shape76()
+{
+  noStroke();
+  fill(88,52,52);
+  quad(298,545,285,458,312,470,335,521);
+}
+function shape77()
+{
+  noStroke();
+  fill(127,86,83);
+  quad(337,543,351,557,372,517,355,514);
+}
+function shape78()
+{
+  noStroke();
+  fill(222,209,197);
+  quad(369,606,354,553,395,506,400,584);
+}
+function shape79()
+{
+  noStroke();
+  fill(195,184,169);
+  triangle(344,174,352,122,405,142);
+}
+function shape80()
+{
+  noStroke();
+  fill(223,212,199);
+  quad(410,208,372,156,445,174,453,217);
+}
+function shape81()
+{
+  noStroke();
+  fill(86,63,48);
+  quad(353,249,338,238,342,219,383,211);
+}
+function shape82()
+{
+  noStroke();
+  fill(182,176,176);
+  quad(370,236,377,244,390,238,389,230);
+}
+function shape83()
+{
+  noStroke();
+  fill(167,134,113);
+  triangle(383,257,418,237,432,252);
+}
+function shape84()
+{
+  noStroke();
+  fill(72,47,33);
+  quad(280,389,256,373,269,352,304,359);
+}
+function shape85()
+{
+  noStroke();
+  fill(38,40,39);
+  triangle(283,443,280,388,331,418);
+}
+function shape86()
+{
+  noStroke();
+  fill(37,26,19);
+  quad(261,587,226,548,225,525,244,526);
+}
+function shape87()
+{
+  noStroke();
+  fill(208,183,179);
+  quad(293,558,293,545,325,534,328,549);
+}
+function shape88()
+{
+  noStroke();
+  fill(172,130,124);
+  triangle(335,521,359,493,312,471);
+}
+function shape89()
+{
+  noStroke();
+  fill(67,45,39);
+  triangle(352,555,368,519,386,518);
+}
+function shape90()
+{
+  noStroke();
+  fill(179,165,154);
+  quad(259,617,205,561,199,518,259,587);
+}
+function shape91()
+{
+  noStroke();
+  fill(47,42,36);
+  triangle(303,370,302,358,337,363);
+}
+function shape92()
+{
+  noStroke();
+  fill(183,154,126);
+  triangle(319,331,356,335,391,450);
+}
+function shape93()
+{
+  noStroke();
+  fill(56,64,63);
+  triangle(331,417,315,410,338,393);
+}
+function shape94()
+{
+  noStroke();
+  fill(26,24,19);
+  quad(250,478,286,460,285,443,229,416);
+}
+function shape95()
+{
+  noStroke();
+  fill(211,196,192);
+  quad(256,551,270,538,247,510,238,510);
+}
+function shape96()
+{
+  noStroke();
+  fill(203,170,175);
+  quad(346,529,347,507,362,490,359,511);
+}
